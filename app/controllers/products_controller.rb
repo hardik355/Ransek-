151b1e5a2d00f2d1e90e71c135edfbd89a0623f8
hardik355 <kapspatel355@gmil.com>
2019-07-22 14:44:45 +0530

    Search by nam,id and other way

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,7 @@ class ProductsController < ApplicationController
   def index
     @search = Product.search(params[:q])
     @products = @search.result
+    @search.build_condition
   end
 
   # GET /products/1
