dbe84c9072a78efd4101da4f4ce18a24732f10b0
hardik355 <kapspatel355@gmil.com>
2019-07-31 16:13:36 +0530

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  root 'posts#index'
-  resources :posts
-
+  resources :products
   resources :tasks
-  # resources :products
-
+  
   resources :products do
     collection { post :search, to: 'products#index' }
   end
